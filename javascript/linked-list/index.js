ebf8651773995e63dcc21363a94e57da7ca95d98
furatmalkawi29 <malkawifurat29@gmail.com>
2021-05-23 04:00:27 +0300

    done8

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,6 +1,44 @@
 'use strict';
 
 class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  add (value) {
+    let node = new Node(value);
+    if (!this.head) {// empty linked list
+      this.head = node;
+    } else {
+      let lastNode = this.head;
+      while (lastNode.next) {
+        lastNode = lastNode.next;
+      }
+      lastNode.next = node;
+    }
+  }
+
+  insert(value) {
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  }
+
+
+  includes(value) {
+    let cursor = this.head;
+    while (!cursor) {
+      if (cursor.data === value) {
+        return true;
+      }
+      cursor = cursor.next;
+    }
+    return false;
+  }
 
 }
 
