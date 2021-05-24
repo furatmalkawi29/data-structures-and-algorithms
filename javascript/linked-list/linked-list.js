6727ef7f942e9edbe00c28d816764b3f37fe2c0d
furatmalkawi29 <malkawifurat29@gmail.com>
2021-05-24 03:38:51 +0300

    linked list2/lab06

--- javascript/linked-list/linked-list.js
+++ javascript/linked-list/linked-list.js
@@ -51,6 +51,57 @@ class LinkedList {
     return temp;
   }
 
+    append(value) {
+    let node = new Node(value);
+    try {
+      if (!this.head) {// empty linked list
+        this.head = node;
+      } else {
+        let lastNode = this.head;
+        while (lastNode.next) {
+          lastNode = lastNode.next;
+        }
+        lastNode.next = node;
+      }
+    }
+    catch (error){
+      console.error('An error occured: ', error);
+    }
+  }
+  insertBefore(value, newVal) {
+    let current = this.head;
+    let previous;
+    while ((current) && (current.value !== value)) {
+      previous = current;
+      current = current.next;
+    }
+    if (!current) {
+      throw `The value ${value}  wasn't found.`;
+    } else {
+      const newNode = new Node(newVal);
+      newNode.next = current;
+      if (current === this.head) {
+        this.head = newNode;
+      } else {
+        previous.next = newNode;
+      }
+    }
+  }
+  insertAfter(value, newVal) {
+    let current = this.head;
+    while ((current) && (current.value !== value)) {
+      current = current.next;
+    }
+    if (!current) {
+      throw `The value ${value}  wasn't found.`;
+    } else {
+      const newNode = new Node(newVal);
+      newNode.next = current.next;
+      current.next = newNode;
+    }
+  }
+
+
 }
 
 module.exports = LinkedList;
