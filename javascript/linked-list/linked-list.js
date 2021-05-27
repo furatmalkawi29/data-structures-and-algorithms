500ff063ddeb6656f0dc9d1fcb6e031b77f5b1d2
furatmalkawi29 <malkawifurat29@gmail.com>
2021-05-27 17:19:25 +0300

    fixed main

--- javascript/linked-list/linked-list.js
+++ javascript/linked-list/linked-list.js
@@ -9,7 +9,7 @@ class LinkedList {
     this.tail = null;
     this.length=0;
   }
-
+  //methods
   insert(data) {
     try {
       let node = new Node(data);
@@ -57,7 +57,88 @@ class LinkedList {
     return list;
   }
 
+  append(value) {
+    let node = new Node(value);
+    if (this.head){
+      let cursor = this.head;
+      while(cursor.next){ //is this the last node?
+        cursor=cursor.next;
+      }
+      cursor.next=node;//let last node points to the new node
+      this.tail = node;
+      this.length++;
 
+    } else { this.head = node;
+      this.tail = node;
+      this.length++;
+
+    }// let head points to new node
+  }
+
+  insertBefore(value,newValue){
+    let node = new Node(newValue);
+    if(this.head){
+      let cursor = this.head;
+      if (cursor.value ===value){
+        node.next = cursor;
+        this.head = node;
+        this.length++;
+
+        return;
+      }
+      while(cursor){
+        if (cursor.next.value===value){
+          node.next = cursor.next;
+          cursor.next = node;
+          this.length++;
+
+          break;
+        }
+        cursor=cursor.next;
+      }
+    } else { this.head = node; // let head points to new node
+      this.tail = node;
+      this.length++;
+    }
+  }
+
+  insertAfter(value, newValue) {
+    let node = new Node(newValue);
+    if(this.head){
+
+      let cursor = this.head;
+      while(cursor){
+        if (cursor.value===value){
+          node.next = cursor.next;
+          cursor.next = node;
+          this.tail = node;
+          this.length++;
+          break;
+        }
+        cursor=cursor.next;}
+    } else { this.head = node; // let head points to new node
+      this.tail = node;
+      this.length++;
+    }
+  }
+
+
+  kthFromEnd(k){
+    if (this.head){
+      if (k < this.length && k >=0 ){
+        let counter = this.length-1;
+
+        let cursor = this.head;
+        while(counter!== k){
+          console.log(cursor.value);
+          cursor=cursor.next;
+          counter--;
+        }
+        return cursor.value;
+
+      } else { return 'k is invalid'; }
+    } else { return 'Empty list.';}
+  }
 }
 
 
@@ -65,3 +146,4 @@ class LinkedList {
 
 
 module.exports = LinkedList;
+
