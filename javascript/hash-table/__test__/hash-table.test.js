13c7bf0bc5283a08e2aa64f46c4c16ed41a89a99
furatmalkawi29 <malkawifurat29@gmail.com>
2021-06-28 14:30:32 +0300

    ok

---
+++ javascript/hash-table/__test__/hash-table.test.js
@@ -0,0 +1,23 @@
+// //>>>>>>>> Testing Hash Table >>>>>>>>>>>>>>>>>>>>>
+// let myhash = new HashTable(1024);
+
+// myhash.set('John', 'Boss');
+// myhash.set('Cathy', 'The Real Boss');
+// myhash.set('Zach', 'Kid 1');
+// myhash.set('Allie', 'Kid 2');
+// myhash.set('Rosie', 'Dog');
+// myhash.set('Cat', 'TA');
+// myhash.set('Justin', 'Student');
+// myhash.set('Jason', 'Student');
+// myhash.set('Ben', 'Student');
+// myhash.set('Timea', 'Student');
+// myhash.set('Jen', 'Student');
+// myhash.set('Khalil', 'Student');
+// myhash.set('Michael', 'Student');
+// myhash.set('Ovi', 'Student');
+// console.log(myhash.get('Zach'));
+// console.log(myhash.contains('Zack'));
+
+// myhash.table.forEach((data, i) => {
+//   console.log(i, data && data.values());
+// });
--- javascript/hash-table/hash-table.js
+++ javascript/hash-table/hash-table.js
@@ -1,5 +1,43 @@
-let LinkedList = require('../linked-list/linked-list');
 
+
+//>>>>>>>> Begin Linked List >>>>>>>>>>>>>>>>>>>>>
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  add(value) {
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = node;
+  }
+
+  values() {
+    let values = [];
+    let current = this.head;
+    while (current) {
+      values.push(current.value);
+      current = current.next;
+    }
+    return values;
+  }
+}
+//>>>>>>>> End Linked List >>>>>>>>>>>>>>>>>>>>>>>>
+//>>>>>>>> Begin Hash Table >>>>>>>>>>>>>>>>>>>>>>>
 
 class HashTable {
   constructor(size) {
@@ -7,30 +45,50 @@ class HashTable {
     this.table = new Array(size);
   }
 
-  hash(key) {// the is a string
+  hash(key) {
     let hash = 0;
+
     hash = key.split('').reduce((result, n) => {
       return result + n.charCodeAt(0);
     }, 0) * 599 % this.size;
-    return hash;
+
+    return hash;//number that will be used as an index
   }
 
   set(key, value) {
+    //claculated the hash number
     let hash = this.hash(key);
+    //check if the cell in array does not have value
+    //then we need to initiate with an empty linked list.
     if (!this.table[hash]) {
       this.table[hash] = new LinkedList();
     }
+    // create new key value pair
     let keyValuePair = { [key]: value };
+    // save the key value into linked list
     this.table[hash].add(keyValuePair);
   }
 
   get(key) {
+    let hash = this.hash(key);
+    if(this.contains(key)){ //check if there's no values at index
+      let bucketValues = this.table[hash].values();
+      let value = bucketValues.filter(item => {
+        for (const property in item) {
+          return property ===key;
+        }
+      });
+      return value[0]?value[0][key]:'Not Found';
+    }
+    return 'Not Found';
   }
 
+
   contains(key) {
-    return true;
+    let hash = this.hash(key);
+
+    return this.table[hash]?true:false;
   }
 }
 
-
-module.exports =HashTable;
+//>>>>>>>> End Hash Table >>>>>>>>>>>>>>>>>>>>>>>>>
